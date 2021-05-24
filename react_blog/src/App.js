2fd581ed30fdabd33db71e36d6e847a8e723c861
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-05-25 02:33:19 +0300

    paulse check 8 not finished

--- react_blog/src/App.js
+++ react_blog/src/App.js
@@ -4,16 +4,21 @@ import React, { useState } from "react";
 
 const App = () => {
   const [posts,setPosts] = useState([{userId:4,id:103,title:"hiii",body:"ahmad"},{userId:5,id:104,title:"We strive to teach you programming",body:"We want you to be a hacker"},]);
-
+  
   const [userId,setUserId]=useState(), [id,setId]=useState(), [title,setTitle]=useState(), [body,setBody]=useState()
   
+  const addPost = () => {
+    setPosts(posts + 1);
+  };
+
+
   return(<>
    <div><h1>Blog App</h1></div>
 
   {posts.map((elem,i)=>{
-return <div key={i}> <h4>{elem.title}</h4> <p>{elem.body}</p>
+return <div key={i}> <h4>{elem.title}</h4> <p>{elem.body}:{addPost}</p>
 
-<button>click here</button>
+<button onClick={addPost}>click here</button>
 <input type ="text" placeholder="Khabis as you wish" onChange={(a)=>{setUserId(a.target.value)}}/>
 <input type ="text" placeholder="Khabis as you wish" onChange={(a)=>{ setId(a.target.value)}}/>
 <input type ="text" placeholder="Khabis as you wish" onChange={(a)=>{setTitle(a.target.value)}}/>
